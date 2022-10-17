15f8c8f0621620112532f62e5021b17ae7b798b0
Sombra-HY <l.sombra.nascimento@gmail.com>
2022-10-17 14:15:13 -0300

    comentario sobre como funcioana o map *81

--- 81_map/DADOS/MAPAMEAMENTO.py
+++ 81_map/DADOS/MAPAMEAMENTO.py
@@ -4,3 +4,8 @@ valores = list(map(lambda x: "R$ %.2f"% x["valor"], produtos))
 produto = list(map(lambda x: "%s, modelo %s custa "% (x["Produto"],x["Modelo"]), produtos))
 
 print(*[x + y for x, y in zip(produto, valores)], sep="\n")
+
+
+# MAP recebe uma funcao ou lambda e um valor ser iterado, tranforma em um iterador
+# ex Map( lambda x :x * 2 , [1,2,3]) ---> cria um iterador 2, 4, 6
+# basta trandormar em lista, caso necessario, list(map(...,...))
