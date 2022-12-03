7d395d9ac21c6c50aa4885a6c625134ec22f3abb
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2022-12-03 19:37:12 -0300

    classes

---
+++ POO/1_Class.py
@@ -0,0 +1,16 @@
+# Class é parecido com um molde ele instacia/ cria um onjeto
+# similar ao struct, estrutura com diversos atributos presentes ( Objeto )
+
+class Pessoa:
+    ...
+
+# Objeto criado
+p1 = Pessoa()
+
+# Atributos do Objeto
+p1.nome = "lucas"
+p1.sobrenome = "Sombra"
+
+
+
+print(p1.nome, p1.sobrenome)
