9eb931dd7f47747688bf9340fe5e7d935b1ca7d6
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2022-12-04 20:15:52 -0300

    Metodo Estatico

---
+++ POO/7_METODOSSTATICS.py
@@ -0,0 +1,14 @@
+# Metodos estaticos
+# segundo o professor é inultiu em Python (nele)
+
+# Basicamente e uma funcao que nao tem nem self nem cls
+# é apenas uma funcao
+
+class Pessoa:
+
+    @staticmethod
+    def ola():
+        print("sla")
+
+
+Pessoa.ola()
